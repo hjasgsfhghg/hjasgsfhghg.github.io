e5c7684b1e348984aa91ebd78ca6903aeed360aa
hjasgsfhghg <132178463+hjasgsfhghg@users.noreply.github.com>
2024-08-01 02:00:29 +0800

    Add files via upload

--- 文字/js/catch.js
+++ 文字/js/catch.js
@@ -1,7 +1,8 @@
 setInterval(function(){
     var cat = localStorage.getItem('cat')
     var fish = localStorage.getItem('fish')
-    if(cat != null){
+    var username = localStorage.getItem('username')
+    if(cat != null && username != ''){
         var add = Number(cat)*Math.random(0.8,1.5)
         say('你的猫为你抓到了' + add.toFixed(0).toString() + '条鱼')
         fish = Number(fish) + Number(add.toFixed(0))
--- 文字/js/main.js
+++ 文字/js/main.js
@@ -1,3 +1,4 @@
+
 function say(word){
     document.getElementById('ul').innerHTML = '<li>' + word.toString() +'</li>' + document.getElementById('ul').innerHTML
 }
@@ -57,35 +58,129 @@ function end(){
 }
 
 function color_night(){
-    document.getElementById('show').style.outline = '2px solid white'
-    document.getElementById('control').style.outline = '2px solid white'
-    document.getElementById('all').style.background = 'black'
+    var text = document.getElementById('color_night').value
+    say('已将颜色切换为' + text)
+    if(text == '夜间'){
+        text = '日间'
+        var colora = 'white'
+        var colorb = 'black'
+    }else{
+        text = '夜间'
+        var colora = 'black'
+        var colorb = 'palegoldenrod'
+    }
+    document.getElementById('color_night').value = text
+    document.getElementById('show').style.outlineColor = colora
+    document.getElementById('control').style.outlineColor = colora
+    document.getElementById('all').style.background = colorb
+    document.getElementById('show').style.color = colora
 }
-function color_leng(){
-    document.getElementById('show').style.outline = '2px solid #2c80c5'
-    document.getElementById('control').style.outline = '2px solid #2c80c5'
-    document.getElementById('all').style.background = '#fffae6'
-}
-function color_nuan(){
-    document.getElementById('show').style.outline = '2px solid #eb7faf'
-    document.getElementById('control').style.outline = '2px solid #eb7faf'
-    document.getElementById('all').style.background = '#eefd6a'
-}
-function color_heihong(){
-    document.getElementById('show').style.outline = '2px solid #e60039'
-    document.getElementById('control').style.outline = '2px solid #e60039'
-    document.getElementById('all').style.background = '#1e3124'
+var username = ''
+localStorage.setItem('username','')
+//键盘监听enter
+document.onkeydown = function(event){
+    if(username==''){
+        return
+    }else{
+        var code = event.keyCode
+        if(code == 13){
+            talka()
+        }
+    }
+    
 }
 
+function save(){
+    var fish = localStorage.getItem('fish');
+    var cat = localStorage.getItem('cat');
+    var msg = {
+        "type":"save",
+        "username":username,
+        "text":{
+            "fish":fish,
+            "cat":cat
+        },
+        "time":time
+    }
+    socket.send(JSON.stringify(msg))
+}
 
+function start(){
+    document.getElementById('start').style.display='';
+    document.getElementById('control').style.display='none'
+}
+function starte(){
+    document.getElementById('start').style.display='none';
+    document.getElementById('control').style.display=''
+}
+
+function joina(){
+    var msg = {
+        type:'join',
+        username:document.getElementById('username').value,
+        password:document.getElementById('password').value,
+        time:time
+    }
+    socket.send(JSON.stringify(msg))
+}
+
+function signa(){
+     var msg = {
+        type:'sign',
+        username:document.getElementById('username').value,
+        password:document.getElementById('password').value,
+        time:time
+    }
+    socket.send(JSON.stringify(msg))
+}
+
+function onlinea(){
+    var msg = {
+        type:'online',
+        time:time
+    }
+    socket.send(JSON.stringify(msg))
+}
+
+function paihanga(){
+    var msg = {
+        type:'paihang',
+        time:time
+    }
+    socket.send(JSON.stringify(msg))
+}
+
+function gonggao(){
+    say('本项目于七月初开始策划并学习相关知识，七月15日开始写前端，七月26日购买并搭建服务器，八月一上传初步成品<br>该项目由 腾讯云提供服务器,github代理网页，仅涉及html,css,js，采用nodejs代理服务器,websocket通讯<br>本项目会持续更新，直至八月26日关停服务器<br>欢迎同学们前来指点不足或提出建议<br>作者：卖的是苦瓜')
+}
 
 //聊天专区
 var socket = new WebSocket('ws://106.52.51.200:3001/')
+var time = new Date().toLocaleDateString()
+
 function talka(){
-    let text = document.getElementById('text')
-    socket.send(text.value)
+    var text = document.getElementById('text').value
+    var msg = {
+        type:'liaotian',
+        text:text,
+        time:time
+    }
+    socket.send(JSON.stringify(msg))
 }
 
 socket.addEventListener('message',function(e){
-    say('【聊天】' + e.data)
+    msg = JSON.parse(e.data)
+    if(msg.type == 'liaotian'){
+        say(msg.text)
+    }else if(msg.type == 'command'){
+        if(msg.text = 'starte'){
+            document.getElementById('start').style.display='none';
+            document.getElementById('control').style.display=''
+        }
+    }else if(msg.type == 'username'){
+        username = msg.text
+        localStorage.setItem('username',username)
+        
+    }
 },100)
+
